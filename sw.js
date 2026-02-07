d2410ccf4d0461907391ca0a35f10789f3b3c1a5
andyl2020 <andyl2020@users.noreply.github.com>
2026-02-07 03:38:22 +0000

    deploy: e46bfc4acecfaa3cb416522b6a87083a40a405e4

---
+++ sw.js
@@ -0,0 +1,56 @@
+const CACHE_NAME = "capybara-quotes-v1";
+const PRECACHE_URLS = [
+  "./",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./icons/icon-512-maskable.png",
+  "./icons/icon-180.png",
+  "./images/capybara-a.jpg",
+  "./images/capybara-b.jpg"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => cache.addAll(PRECACHE_URLS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      const fetchPromise = fetch(event.request)
+        .then((response) => {
+          if (response && response.status === 200) {
+            const responseClone = response.clone();
+            caches.open(CACHE_NAME).then((cache) => cache.put(event.request, responseClone));
+          }
+          return response;
+        })
+        .catch(() => {
+          if (event.request.mode === "navigate") {
+            return caches.match("./");
+          }
+          return cached;
+        });
+
+      return cached || fetchPromise;
+    })
+  );
+});
